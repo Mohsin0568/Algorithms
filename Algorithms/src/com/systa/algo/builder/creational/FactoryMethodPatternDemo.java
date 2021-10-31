b31df0983dc8776c9e390476c4abe156733efd24
Mohsin0568 <mohsin68.murtuza@gmail.com>
2021-10-31 12:18:46 +0530

    added code for factory method implementation

---
+++ Algorithms/src/com/systa/algo/builder/creational/FactoryMethodPatternDemo.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.systa.algo.builder.creational;
+
+/**
+ * @author mohsin
+ * 
+ * the factory method is defined as providing an interface for object creation but delegating the actual instantiation of objects to subclasses.
+ *
+ */
+
+class Request{
+	private String requestNo;
+
+	@Override
+	public String toString() {
+		return "Request [requestNo=" + requestNo + "]";
+	}
+}
+
+class Response{
+	
+	private String responseCode;
+	Response(String responseCode){
+		this.responseCode = responseCode;
+	}
+	
+	@Override
+	public String toString() {
+		return "Response [responseCode=" + responseCode + "]";
+	}	
+}
+
+interface CustomerInformationSystem{
+	Response makeCallToCIS(Request request);
+}
+
+class WBImplementation implements CustomerInformationSystem{
+
+	@Override
+	public Response makeCallToCIS(Request request) {
+		// make call to CIS via WB
+		return new Response("0000");
+	}	
+}
+
+class MadameImplementation implements CustomerInformationSystem{
+	
+	@Override
+	public Response makeCallToCIS(Request request) {
+		// make call to CIS via WB
+		return new Response("9999");
+	}
+}
+
+class CISImplementationFactory {
+	
+	public static CustomerInformationSystem getImplementationClass(String type) {
+		if(type.equalsIgnoreCase("WB")) {
+			return new WBImplementation();
+		}
+		else if (type.equals("MADAMME")) {
+			return new MadameImplementation();
+		}
+		return null;
+	}
+}
+
+public class FactoryMethodPatternDemo {
+
+	public static void main(String args[]) {
+		CustomerInformationSystem system = CISImplementationFactory.getImplementationClass("WB");
+		System.out.println(system.makeCallToCIS(new Request()));
+		
+		CustomerInformationSystem system1 = CISImplementationFactory.getImplementationClass("MADAMME");
+		System.out.println(system1.makeCallToCIS(new Request()));
+	}
+	
+}
